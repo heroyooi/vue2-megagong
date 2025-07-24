262ccf5d2bb1f3c59eb73901397fe8dab34a5c17
heroyooi <heroyooi1018@gmail.com>
2025-07-24 17:59:05 +0900

    2강 과제: MainSlide.vue 구현 (Swiper)

--- src/components/MainSlide.vue
+++ src/components/MainSlide.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="main-slide">
-    <div class="slide-banner">[ 여기에 슬라이드 배너가 들어갑니다 ]</div>
+    <swiper :options="swiperOption" class="swiper-container">
+      <swiper-slide v-for="(item, i) in slides" :key="i">
+        <img :src="item.img" :alt="item.alt" class="slide-img" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
   </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  name: "MainSlide",
+  data() {
+    return {
+      slides: [
+        { img: "https://img.megagong.net/m/2025/0724_pass/gong/pass_gong.png", alt: "배너1" },
+        { img: "https://img.megagong.net/m/2025/0724_pass/tech/pass_tech.png", alt: "배너2" },
+        { img: "https://img.megagong.net/m/2025/0724_pass/army/pass_army.png", alt: "배너3" },
+      ],
+      swiperOption: {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style>
 .main-slide {
-  background: #eaf3ff;
-  height: 300px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 12px;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+  width: 790px;
 }
 </style>

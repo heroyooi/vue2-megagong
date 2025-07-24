bec265507026f61ef835e9dc3632db2ae8657eb9
heroyooi <heroyooi1018@gmail.com>
2025-07-24 15:00:05 +0900

    1강: 실전 프로젝트 구조 설계 & 초기 세팅

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // 백엔드 API 서버 주소
+        changeOrigin: true,
+      },
+    },
+  },
+};

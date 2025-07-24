d1ce0169632d0d715e5771e30d298b2f65a61c60
heroyooi <heroyooi1018@gmail.com>
2025-07-24 17:20:56 +0900

    2강: 메인 레이아웃 구조 퍼블리싱 (헤더 + 퀵메뉴 + 본문 좌우 콘텐츠)

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div>
+    <MainHeader />
     <router-view />
+    <MainFooter />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MainHeader from "@/components/Header.vue";
+import MainFooter from "@/components/Footer.vue";
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  components: {
+    MainHeader,
+    MainFooter,
+  },
+};
+</script>

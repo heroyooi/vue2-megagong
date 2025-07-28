3ddb96e8c1f96d0acbf5e95f34295822aea4e930
heroyooi <heroyooi1018@gmail.com>
2025-07-28 14:48:00 +0900

    3강 과제. 메인 탭 + 슬라이드 연동 UI 구현

--- src/components/TabSlide.vue
+++ src/components/TabSlide.vue
@@ -4,6 +4,7 @@
       <div class="teacher-box">
         <img :src="item.img" :alt="item.alt" />
         <p class="teacher-name">{{ item.alt }}</p>
+        <p class="teacher-subject">{{ item.subject }}</p>
       </div>
     </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
@@ -27,6 +28,12 @@ export default {
           el: ".swiper-pagination",
         },
         loop: false,
+        breakpoints: {
+          1200: { slidesPerView: 4 },
+          1024: { slidesPerView: 3 },
+          768: { slidesPerView: 2 },
+          480: { slidesPerView: 1 },
+        },
       },
     };
   },
@@ -83,5 +90,11 @@ export default {
   font-size: 14px;
   font-weight: 600;
   color: #333;
+}
+
+.teacher-subject {
+  font-size: 13px;
+  color: #666;
+  margin-top: 4px;
 }
 </style>
